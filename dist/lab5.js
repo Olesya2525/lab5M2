16cb7ec05e612e9958ac224f5326019b34a1e622
Olesya2525 <olesya.ryabets@icloud.com>
2025-02-21 18:29:54 +0400

    Add files via upload

---
+++ dist/lab5.js
@@ -0,0 +1,26 @@
+function getCurrentDate() {
+    console.log(Date.now());
+}
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Загрузилось");
+    function getCurrentDate() {
+        console.log(Date.now());
+    }
+    setTimeout(() => {
+        clearInterval(intervalDescriptor);
+        getCurrentDate();
+        const intFunction = extFunction("мяу");
+        intFunction("2507");
+    }, 10000);
+    let sec = 1;
+    let intervalDescriptor = setInterval(() => {
+        console.log("Время: " + sec);
+        setTimeout(() => {
+            sec++;
+        }, 1);
+    }, 999);
+    function extFunction(x) {
+        console.log("Внешняя фунция вводим строку: " + x);
+        return (y) => console.log("Возврат результата в виде функции с замыканием на внешнюю функцию: " + (x + y));
+    }
+});
